fb0cb4cc2c35c7b783ef9750bc461d542f70b652
biswa baral <131336755+biswabaral417@users.noreply.github.com>
2023-04-30 18:48:34 +0545

    added buttons to chose betwenn in rps to play

--- rockpaperscissor/index.js
+++ rockpaperscissor/index.js
@@ -1,64 +1,51 @@
-let user_choice=prompt("enter rock paper or scissor");
-function a()
-{
-    rndm=Math.floor(Math.random()*100);
-    if (rndm>=66) {
+let computer_choice = "";
+let user_choice = "";
+function a() {
+    rndm = Math.floor(Math.random() * 100);
+    if (rndm >= 66) {
         return "rock";
     }
-    else if(rndm>=33){
+    else if (rndm >= 33) {
         return "scissor";
     }
-    else{
+    else {
         return "paper";
     }
 }
-let helo="";
-helo=a();
-
-
-function userChoice(){
-    switch (user_choice) {
-        case "rock":
-            if (helo=="scissor") {
-                alert("u win");
-            }
-            else if(helo=='paper'){
-                alert('u lose')
-            }
-            else{
-                alert("draw")
-            }
-            break;
-            
-            case "paper":
-                if (helo=="rock") {
-                    alert("u win");
-                }
-                else if(helo=='scissor'){
-                    alert('u lose')
-                }
-                else{
-                    alert("draw")
-                }
-            
-            break;
-            
-            case "scissor":
-                if (helo=="paper") {
-                    alert("u win");
-                }
-                else if(helo=='rock'){
-                    alert('u lose')
-                }
-                else{
-                    alert("draw")
-                }
-            
-            break;
-        default:
-            alert("wrong choices try reloading page and then enter rock paper or scissor");
-            break;
+function compare() {
+    if (user_choice == computer_choice) {
+        alert("draw");
+    }
+    else if (user_choice == "rock" && computer_choice == "scissor") {
+        alert('u win');
+    }
+    else if (user_choice == "paper" && computer_choice == "rock") {
+        alert('u win');
+    }
+    else if(user_choice == "scissor" && computer_choice == "paper"){
+        alert('u win');
+    }
+    else {
+        alert('u lose');
     }
 
 }
-userChoice();
+
+
+
+function rock() {
+    user_choice = "rock";
+    computer_choice = a();
+    compare();
+
+}
+function scissor() {
+    user_choice = "scissor";
+    computer_choice = a();
+    compare();
+}
+function paper() {
+    user_choice = "paper";
+    computer_choice = a();
+    compare();
+}
