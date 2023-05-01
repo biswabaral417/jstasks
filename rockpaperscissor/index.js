7a14ee087d140a9d10c4df707ac815ae781d71c6
biswa baral <131336755+biswabaral417@users.noreply.github.com>
2023-05-01 19:16:37 +0545

    rock paper scssor is now very cool

--- rockpaperscissor/index.js
+++ rockpaperscissor/index.js
@@ -1,51 +1,73 @@
 let computer_choice = "";
 let user_choice = "";
+let rbtn=document.querySelector('#rbtn');
+let pprbtn=document.querySelector('#pprbtn');
+let scbtn=document.querySelector('#scbtn');
+let crbtn=document.querySelector('#crbtn');
+let cpprbtn=document.querySelector('#cpprbtn');
+let cscbtn=document.querySelector('#cscbtn');
+let resulttxt=document.querySelector('#resulttxt')
 function a() {
     rndm = Math.floor(Math.random() * 100);
     if (rndm >= 66) {
+        crbtn.style.border="10px solid red"
+        cpprbtn.style.border="none"
+        cscbtn.style.border="none"
         return "rock";
     }
     else if (rndm >= 33) {
+        cscbtn.style.border="10px solid red"
+        crbtn.style.border="none"
+        cpprbtn.style.border="none"
         return "scissor";
     }
     else {
+        cpprbtn.style.border="10px solid red"
+        cscbtn.style.border="none"
+        crbtn.style.border="none"
         return "paper";
     }
 }
 function compare() {
     if (user_choice == computer_choice) {
-        alert("draw");
+        resulttxt.innerText="draw";
     }
     else if (user_choice == "rock" && computer_choice == "scissor") {
-        alert('u win');
+        resulttxt.innerText='u win';
     }
     else if (user_choice == "paper" && computer_choice == "rock") {
-        alert('u win');
+        resulttxt.innerText='u win';
     }
     else if(user_choice == "scissor" && computer_choice == "paper"){
-        alert('u win');
+        resulttxt.innerText='u win';
     }
     else {
-        alert('u lose');
+        resulttxt.innerText='u lose';
     }
 
 }
-
-
 
 function rock() {
     user_choice = "rock";
+    rbtn.style.border="10px solid green"
+    pprbtn.style.border="none"
+    scbtn.style.border="none"
     computer_choice = a();
     compare();
-
 }
 function scissor() {
     user_choice = "scissor";
+    scbtn.style.border="10px solid green"
+    rbtn.style.border="none"
+    pprbtn.style.border="none"
     computer_choice = a();
     compare();
 }
 function paper() {
     user_choice = "paper";
+    pprbtn.style.border="10px solid green"
+    scbtn.style.border="none"
+    rbtn.style.border="none"
     computer_choice = a();
     compare();
 }
